7d84362f3574fab2a0c88aa4ec137cacb316220b
bayuforge <bayuanataw@gmail.com>
2026-03-07 01:40:35 +0800

    Add function to calculate area of a circle

---
+++ part_one/lingkaran.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+#include "lingkaran.h"
+
+float area_of_circle(float r){
+        double  pi = M_PI; //M_PI is cmath built in PI Constant
+
+        double  res = pi * (r * r);
+
+        return res;
+}
